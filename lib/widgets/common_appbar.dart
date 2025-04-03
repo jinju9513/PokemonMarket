8fd77b0c07eef61acadcea3ab7890fbaca8836ee
hippieperm <leess0133@gmail.com>
2025-04-03 17:05:54 +0900

    CommonAppbar 위젯 추가: 다크 모드 토글 버튼을 포함한 상단 앱바 구현

---
+++ lib/widgets/common_appbar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:pokemon_market/widgets/common_text.dart';
+
+class CommonAppbar extends StatelessWidget {
+  final bool isDarkMode;
+  final VoidCallback toggleTheme;
+
+  const CommonAppbar({
+    super.key,
+    required this.isDarkMode,
+    required this.toggleTheme,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: CommonText(
+        text: 'POKE',
+        fontSize: 24,
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(
+            isDarkMode ? Icons.wb_sunny : Icons.nightlight_round,
+            color: isDarkMode ? Colors.yellow : Colors.blueGrey,
+          ),
+          onPressed: toggleTheme,
+        ),
+      ],
+    );
+  }
+}
