d44cfa4980626d197dbcae87c1777c2918771164
hippieperm <leess0133@gmail.com>
2025-04-03 16:14:19 +0900

    홈페이지 상태 관리 추가: 다크 모드 전환 기능 구현 및 테마 적용

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,34 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pokemon_market/widgets/common_text.dart';
 import 'package:pokemon_market/widgets/home_page/home_page_list.dart';
+import 'package:flutter/rendering.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isDarkMode = false;
+
+  void toggleTheme() {
+    setState(() {
+      isDarkMode = !isDarkMode;
+      // 테마 변경 로직 - 실제 앱 테마 변경
+      final brightness = isDarkMode ? Brightness.dark : Brightness.light;
+      final systemUiOverlayStyle = SystemUiOverlayStyle(
+        statusBarBrightness: brightness,
+        systemNavigationBarIconBrightness: brightness,
+        statusBarIconBrightness:
+            brightness == Brightness.light ? Brightness.dark : Brightness.light,
+      );
+      SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: CommonText(
-          text: 'POKE',
-          fontSize: 24,
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            //TODO: 삼항연산자 사용해서 상품여부 확인필요
-            CommonText(
-              text: '상품이 없습니다.',
-              fontSize: 24,
+    return Theme(
+      data: isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: CommonText(
+            text: 'POKE',
+            fontSize: 24,
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(
+                isDarkMode ? Icons.wb_sunny : Icons.nightlight_round,
+                color: isDarkMode ? Colors.yellow : Colors.blueGrey,
+              ),
+              onPressed: toggleTheme,
             ),
-            HomePageList(),
           ],
         ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              //TODO: 삼항연산자 사용해서 상품여부 확인필요
+              CommonText(
+                text: '상품이 없습니다.',
+                fontSize: 24,
+              ),
+              HomePageList(),
+            ],
+          ),
+        ),
+        floatingActionButton: Fab(),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       ),
-      floatingActionButton: Fab(),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 
